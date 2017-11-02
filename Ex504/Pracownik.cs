cd662ecbb3389c3e4d65f7535b02df444f8e0fd7
Piotrek <@>
2017-11-02 23:05:37 +0100

    przesłanie plików

---
+++ Ex504/Pracownik.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex504
+{
+    class Pracownik : IComparable
+    {
+        private int wynagrodzenie;
+        private string imie;
+
+        public int CompareTo(Object obj)
+        {
+            Pracownik other = obj as Pracownik;  //traktowanie obj jak pracownika
+            if (this.wynagrodzenie == other.wynagrodzenie)
+            {
+                return this.imie.CompareTo(other.imie);
+            }
+            return other.wynagrodzenie.CompareTo(this.wynagrodzenie);
+        }
+
+        public Pracownik(string imie, int wynagrodzenie) //konstruktor
+        {
+            this.imie = imie;
+            this.wynagrodzenie = wynagrodzenie;
+        }
+
+        public override string ToString() //przesłonięcie metody
+        {
+            return this.imie + " " + this.wynagrodzenie;
+        }
+
+    }
+}
---
+++ Ex504/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex504
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList pracownicy = new ArrayList();
+            pracownicy.Add(new Pracownik("Jan", 3000));
+            pracownicy.Add(new Pracownik("Anna", 2500));
+            pracownicy.Add(new Pracownik("Jan", 3200));
+            pracownicy.Add(new Pracownik("Zofia", 4100));
+            pracownicy.Add(new Pracownik("Barbara", 4100));
+            pracownicy.Sort();
+            foreach (var element in pracownicy)
+            {
+                Console.WriteLine(element.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
